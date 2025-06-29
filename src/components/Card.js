e59e37a7cd5293ad1e97accfe3b6a3d5fea81010
Ayushman-mnnit <araj53062@gmail.com>
2025-06-30 00:19:28 +0530

    Added image modal preview on card image click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,57 @@
 import { useState } from "react";
 
+function Card({ id, image, info, price, name, removeTour }) {
+  const [readmore, setReadmore] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-function Card({id, image,info,price,name, removeTour})  {
-    const[readmore,setReadmore] = useState(false);
-    
-    const description = readmore ? info :`${info.substring(0,200)}....`;
-    function readmoreHandler() {
-        setReadmore(!readmore);
-    }
+  const description = readmore ? info : `${info.substring(0, 200)}....`;
 
+  function readmoreHandler() {
+    setReadmore(!readmore);
+  }
 
-    return (
-        
-        <div className="card">
+  return (
+    <>
+      <div className="card">
+        <img
+          src={image}
+          className="image"
+          alt="tour"
+          onClick={() => setShowModal(true)}
+        />
 
-            <img src={image} className="image"></img>
+        <div className="tour-info">
+          <div className="tour-details">
+            <h4 className="tour-price">₹ {price}</h4>
+            <h4 className="tour-name">{name}</h4>
+          </div>
 
-            <div className="tour-info">
-                <div className="tour-details">
-                    <h4 className="tour-price">₹ {price}</h4>
-                    <h4 className="tour-name">{name}</h4>
-                </div>
-
-                <div className="description">
-                    {description}
-                    <span className="read-more" onClick={readmoreHandler}>
-                        {readmore ? `Show Less`: `Read \More`}
-                    </span>
-                </div>
-            </div>
-
-            <button className="btn-red" onClick={() => removeTour(id)}>
-                Not Interested
-            </button>
+          <div className="description">
+            {description}
+            <span className="read-more" onClick={readmoreHandler}>
+              {readmore ? `Show Less` : `Read More`}
+            </span>
+          </div>
         </div>
-        
-    );
+
+        <button className="btn-red" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </div>
+
+      {/* Modal */}
+      {showModal && (
+        <div className="modal-overlay" onClick={() => setShowModal(false)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="modal-close" onClick={() => setShowModal(false)}>
+              ✖
+            </span>
+            <img src={image} alt="Zoomed Tour" className="modal-img" />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Card;
